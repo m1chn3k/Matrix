b2b10315505f2dccfb7bb08f72a0e9bbc99c6526
m1chn3k <m1chn3k@gmail.com>
2025-06-04 21:09:15 +0300

    snl redact

---
+++ MatrixSum/MatrixSum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public partial class Program
+{
+    static void Main()
+    {
+        int[,] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9},
+            {10, 11, 12}
+        };
+
+        int sum = MatrixSummator.ForkJoinSum(matrix);
+        Console.WriteLine($"Total sum of matrix elements is {sum}");
+    }
+}
