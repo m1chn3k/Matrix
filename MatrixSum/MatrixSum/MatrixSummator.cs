79d44049c78194f7208d6028eb590d10012b63e6
m1chn3k <m1chn3k@gmail.com>
2025-06-04 21:26:30 +0300

    Replace busy-wait loop with CountdownEvent for thread synchronization

--- MatrixSum/MatrixSum/MatrixSummator.cs
+++ MatrixSum/MatrixSum/MatrixSummator.cs
@@ -11,13 +11,7 @@ public static class MatrixSummator
         int[] rowSums = new int[rows];
         int totalSum = 0;
 
-        Barrier barrier = new Barrier(rows, b =>
-        {
-            int sum = 0;
-            for (int i = 0; i < rowSums.Length; i++)
-                sum += rowSums[i];
-            totalSum = sum;
-        });
+        var countdown = new CountdownEvent(rows);
 
         for (int i = 0; i < rows; i++)
         {
@@ -27,14 +21,17 @@ public static class MatrixSummator
                 int sum = 0;
                 for (int j = 0; j < matrix.GetLength(1); j++)
                     sum += matrix[row, j];
+
                 rowSums[row] = sum;
-                barrier.SignalAndWait();
+
+                countdown.Signal();
             }).Start();
         }
 
-        // Чекаємо, поки всі потоки закінчать і totalSum оновиться
-        while (totalSum == 0 && rows > 0)
-            Thread.Sleep(1);
+        countdown.Wait();
+
+        for (int i = 0; i < rowSums.Length; i++)
+            totalSum += rowSums[i];
 
         return totalSum;
     }
